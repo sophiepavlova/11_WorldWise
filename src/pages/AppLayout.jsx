eaa7eb2970971a6ed794c3c10184a867a3cfeba7
sophiepavlova <olni.pavlova@gmail.com>
2026-07-10 14:03:10 -0700

    Style the sidebar in AppLayout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,10 @@
-import AppNav from '../components/AppNav';
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css';
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <h1>APP</h1>
+    <div className={styles.app}>
+      <Sidebar />
     </div>
   );
 }
