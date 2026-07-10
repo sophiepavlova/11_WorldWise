553a7e98074d918530854558aa33608d6e272e1b
sophiepavlova <olni.pavlova@gmail.com>
2026-07-10 13:44:04 -0700

    Implement and style PageNav and added many components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage.jsx';
 import Product from './pages/Product.jsx';
 import Pricing from './pages/Pricing.jsx';
-import Homepage from './pages/Homepage.jsx';
-import PageNotFound from './pages/PageNotFound.jsx';
+import Login from './pages/Login.jsx';
 import AppLayout from './pages/AppLayout.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />} />
-        <Route path='/' element={<Homepage />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
